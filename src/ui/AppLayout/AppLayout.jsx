4b4583e158d17b6c273040b1aa98626258aff3c9
Rachid Bendaghor <rachidbendaghor@gmail.com>
2023-10-21 23:17:37 +0100

    added logic and loader and component for dispaling a breeds details. #9

--- src/ui/AppLayout/AppLayout.jsx
+++ src/ui/AppLayout/AppLayout.jsx
@@ -1,10 +1,10 @@
 import { Outlet } from 'react-router';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
-
+import styles from './AppLayout.module.css';
 export default function AppLayout() {
   return (
-    <div>
+    <div className={`${styles.appLayout}`}>
       <Header />
       <main>
         <Outlet />
